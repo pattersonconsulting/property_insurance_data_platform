481d19e72edc9685223142518352c3613c443b82
Josh Patterson <jpatterson@floe.tv>
2025-05-29 15:05:08 -0400

    sql script for s3 to dbx load

---
+++ airflow_data_pipelines/bordereaux_gold_layer_update/load_s3_bordereaux_into_dbx_airflow.sql
@@ -0,0 +1,27 @@
+
+copy into  claims_workspace.bronze.bordereaux_data 
+from 
+    (
+        SELECT 
+        policy_number,
+        CAST(months_insured as BIGINT) as months_insured,
+        has_claims,
+        CAST(items_insured as BIGINT) as items_insured,
+        claim_reference,
+        insured_name,
+        CAST(policy_start_date as DATE) as policy_start_date,
+        CAST(date_of_loss as DATE) as date_of_loss,
+        CAST(date_reported as DATE) as date_reported,
+        claim_status,
+        loss_type,
+        paid_amount,
+        reserve_amount,
+        total_incurred,
+        claim_propensity
+        
+
+        FROM 's3://property-insurance-examples/test-bordereaux-data/daily_bordereaux_merged_data/'
+    )
+FILEFORMAT = CSV FORMAT_OPTIONS('sep' = ',', 'header' = 'true', 'inferSchema' = 'true')
+COPY_OPTIONS ('force' = 'true', 'mergeSchema' = 'true')
+
